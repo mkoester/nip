0921baf02ff4b10ff5424b7ae38c654bedc474cd
Mirko Köster <it@mirkokoester.de>
2024-03-09 20:03:29 +0100

    sidebar for small screens (drawer)

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,11 +1,23 @@
-<nav class="list-nav">
+<script lang="ts">
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
+
+	const drawerStore = getDrawerStore();
+
+	function drawerClose(): void {
+		drawerStore.close();
+	}
+</script>
+
+<nav class="list-nav p-4">
 	<ul>
-		<li><a href="/">Homepage</a></li>
-		<li><a href="/about" class="line-through">About</a></li>
-		<li><a href="/question/1">Q1</a></li>
-		<li><a href="/question/1/answers">Q1 + answers</a></li>
-		<li><a href="/question/2/answers">Q2 + answers</a></li>
-		<li><a href="/question/3/answers">Q3 + answers</a></li>
-		<li><a href="/question/4/answers" class="line-through">Q4 + answers</a></li>
+		<li><a on:click={drawerClose} href="/">Home</a></li>
+		<li><a on:click={drawerClose} href="/about" class="line-through">About</a></li>
+		<li><a on:click={drawerClose} href="/question/1">Q1</a></li>
+		<li><a on:click={drawerClose} href="/question/1/answers">Q1 + answers</a></li>
+		<li><a on:click={drawerClose} href="/question/2/answers">Q2 + answers</a></li>
+		<li><a on:click={drawerClose} href="/question/3/answers">Q3 + answers</a></li>
+		<li>
+			<a on:click={drawerClose} href="/question/4/answers" class="line-through">Q4 + answers</a>
+		</li>
 	</ul>
 </nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
 	import Navigation from '$lib/Navigation.svelte';
 	import '../app.postcss';
+
+	initializeStores();
+	const drawerStore = getDrawerStore();
+
+	function drawerOpen(): void {
+		drawerStore.open({});
+	}
 </script>
 
-<AppShell>
+<Drawer>
+	<h2 class="p-4">Navigation</h2>
+	<hr />
+	<Navigation />
+</Drawer>
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
 	<svelte:fragment slot="header">
-		<AppBar>I don't know a single person who is perfect</AppBar>
+		<div class="flex items-center">
+			<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+				<span>
+					<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+						<rect width="100" height="20" />
+						<rect y="30" width="100" height="20" />
+						<rect y="60" width="100" height="20" />
+					</svg>
+				</span>
+			</button>
+			<strong class="text-xl">I don't know a single person who is perfect</strong>
+		</div>
 	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
 		<Navigation />
