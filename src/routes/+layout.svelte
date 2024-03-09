df1fd57993de20f106c9c84638a0de91a3d9dfcc
Mirko Köster <it@mirkokoester.de>
2024-03-09 20:25:40 +0100

    oh, this is how you are supposed to use lead slot

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
-	import { AppShell, initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
+	import {
+		AppShell,
+		AppBar,
+		initializeStores,
+		Drawer,
+		getDrawerStore
+	} from '@skeletonlabs/skeleton';
 	import Navigation from '$lib/Navigation.svelte';
 	import '../app.postcss';
 
@@ -16,20 +22,24 @@
 	<hr />
 	<Navigation />
 </Drawer>
-<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-48">
 	<svelte:fragment slot="header">
-		<div class="flex items-center">
-			<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
-				<span>
-					<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-						<rect width="100" height="20" />
-						<rect y="30" width="100" height="20" />
-						<rect y="60" width="100" height="20" />
-					</svg>
-				</span>
-			</button>
-			<strong class="text-xl">I don't know a single person who is perfect</strong>
-		</div>
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<div class="flex items-center">
+					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
+					</button>
+					<strong class="text-xl">I don't know a single person who is perfect</strong>
+				</div>
+			</svelte:fragment>
+		</AppBar>
 	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
 		<Navigation />
