8f0736ade6bb119012a8d13fcecffb2db27383e8
Mirko Köster <it@mirkokoester.de>
2024-03-09 01:27:37 +0100

    fmt

--- src/lib/db/sqlite.ts
+++ src/lib/db/sqlite.ts
@@ -10,10 +10,20 @@ async function openDb(): Promise<Database> {
 	});
 }
 
-export async function get_question(game: number, question_id: number): Promise<Question | undefined> {
-	return openDb().then((db) => db.get<Question>("SELECT id, lang, question FROM questions WHERE id = ?", question_id));
+export async function get_question(
+	game: number,
+	question_id: number
+): Promise<Question | undefined> {
+	return openDb().then((db) =>
+		db.get<Question>('SELECT id, lang, question FROM questions WHERE id = ?', question_id)
+	);
 }
 
 export async function get_answers(game: number, question_id: number): Promise<Answer[]> {
-	return openDb().then((db) => db.all<Answer[]>("SELECT id, answer FROM answers WHERE question_id = ? ORDER BY id", question_id));
+	return openDb().then((db) =>
+		db.all<Answer[]>(
+			'SELECT id, answer FROM answers WHERE question_id = ? ORDER BY id',
+			question_id
+		)
+	);
 }
--- src/routes/question/[id]/answers/+page.server.ts
+++ src/routes/question/[id]/answers/+page.server.ts
@@ -6,7 +6,7 @@ export async function load({ parent, params }): Promise<QnA> {
 	const id = Number(params.id);
 	return get_answers(1, id).then((a) => {
 		const q: Promise<Question> = parent();
-		const answers: Answer[] = a
+		const answers: Answer[] = a;
 		const res: Promise<QnA> = q.then((question: Question) => ({
 			question,
 			answers
