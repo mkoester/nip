2f545b50314db3705bcdca516eabe052692aeda0
Mirko Köster <it@mirkokoester.de>
2024-03-09 19:41:53 +0100

    using AppShell with a nav bar (left)

---
+++ src/lib/Navigation.svelte
@@ -0,0 +1,11 @@
+<nav class="list-nav">
+	<ul>
+		<li><a href="/">Homepage</a></li>
+		<li><a href="/about" class="line-through">About</a></li>
+		<li><a href="/question/1">Q1</a></li>
+		<li><a href="/question/1/answers">Q1 + answers</a></li>
+		<li><a href="/question/2/answers">Q2 + answers</a></li>
+		<li><a href="/question/3/answers">Q3 + answers</a></li>
+		<li><a href="/question/4/answers" class="line-through">Q4 + answers</a></li>
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/Navigation.svelte';
 	import '../app.postcss';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>I don't know a single person who is perfect</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<Navigation />
+	</svelte:fragment>
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
