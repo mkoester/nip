a93bd5512c37f8b2f0308ba794ba459864363aba
Mirko Köster <it@mirkokoester.de>
2024-03-14 21:16:07 +0100

    properly typed locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+
+import type { User } from '$lib/types';
+
+declare global {
+	namespace App {
+		interface Locals {
+			user: User | undefined;
+		}
+		// interface PageData {}
+		// interface Error {}
+		// interface Platform {}
+	}
 }
