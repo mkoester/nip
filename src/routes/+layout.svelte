ea9c4f3d170833e0fbc73ebc908449e9296ab1d0
Mirko Köster <it@mirkokoester.de>
2024-04-03 16:59:53 +0200

    moved 'loading translations' hint to the footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,7 @@
 		Avatar,
 		initializeStores,
 		Drawer,
-		getDrawerStore,
-		ProgressRadial
+		getDrawerStore
 	} from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 	import Navigation from '$lib/Navigation.svelte';
@@ -24,56 +23,53 @@
 	export let data: UserInformation & { locale: string };
 </script>
 
-{#await loadTranslations(data.locale, $page.url.pathname)}
-	<div class="grid h-screen v-screen place-items-center">
-		<ProgressRadial />
-		loading ...
-	</div>
-{:then}
-	<Drawer>
-		<h2 class="p-4">Navigation</h2>
-		<hr />
+<Drawer>
+	<h2 class="p-4">Navigation</h2>
+	<hr />
+	<Navigation />
+</Drawer>
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-48">
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<div class="flex items-center">
+					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
+					</button>
+					<strong class="text-xl">{$t('header.title')}</strong>
+				</div>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				{#if data?.user}
+					<form method="POST">
+						<button formaction="/user?/logout"
+							><Avatar initials={data?.user.username} background="bg-primary-500" /></button
+						>
+					</form>
+				{:else}
+					<span class="badge variant-filled"><a href="/user/login">Log in</a></span>
+				{/if}
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
 		<Navigation />
-	</Drawer>
-	<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-48">
-		<svelte:fragment slot="header">
-			<AppBar>
-				<svelte:fragment slot="lead">
-					<div class="flex items-center">
-						<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
-							<span>
-								<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-									<rect width="100" height="20" />
-									<rect y="30" width="100" height="20" />
-									<rect y="60" width="100" height="20" />
-								</svg>
-							</span>
-						</button>
-						<strong class="text-xl">{$t('header.title')}</strong>
-					</div>
-				</svelte:fragment>
-				<svelte:fragment slot="trail">
-					{#if data?.user}
-						<form method="POST">
-							<button formaction="/user?/logout"
-								><Avatar initials={data?.user.username} background="bg-primary-500" /></button
-							>
-						</form>
-					{:else}
-						<span class="badge variant-filled"><a href="/user/login">Log in</a></span>
-					{/if}
-				</svelte:fragment>
-			</AppBar>
-		</svelte:fragment>
-		<svelte:fragment slot="sidebarLeft">
-			<Navigation />
-		</svelte:fragment>
-		<!-- (sidebarRight) -->
-		<!-- (pageHeader) -->
-		<!-- Router Slot -->
-		<slot />
-		<!-- ---- / ---- -->
-		<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
-		<!-- (footer) -->
-	</AppShell>
-{/await}
+	</svelte:fragment>
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">
+		{#await loadTranslations(data.locale, $page.url.pathname)}
+			<div class="grid place-items-center">loading translations...</div>
+		{/await}
+	</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
